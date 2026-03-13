6ef82b61dd028ee25c31d5e3d6761b42147502c3
lhc14723 <lhckkll@163.com>
2026-03-13 17:48:59 +0800

    finish

--- forum/urls.py
+++ forum/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  # 确保导入了 include
+from django.urls import path, include  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('boards.urls')),  # ← 必须有这行，且前缀是 api/
+    path('api/', include('boards.urls')),  
 ]
